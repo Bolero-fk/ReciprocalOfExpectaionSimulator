7b93a1b4d2b7c5a0413bac90b4dc5619a8ee5305
FukudaKento <fuku_0000@outlook.jp>
2022-11-14 22:06:27 +0900

    時間によってグラフが変化するようにした

--- DrawGraph.js
+++ DrawGraph.js
@@ -1,21 +1,45 @@
-chart: Chart;
-function InitializeGraph() {
-    var ctx = document.getElementById('ex_chart');
-    var data = {
-        datasets: [{
-                radius: 0,
-                label: "フーリエ関数",
-                showLine: true,
-                fill: false,
-                borderColor: 'rgb(255, 99, 132)',
-                backgroundColor: 'rgb(255, 99, 132)'
-            }]
+var GraphDrawer = /** @class */ (function () {
+    function GraphDrawer() {
+    }
+    GraphDrawer.prototype.InitializeGraph = function (contextId) {
+        var ctx = document.getElementById(contextId);
+        var data = {
+            datasets: [{
+                    radius: 0,
+                    label: "フーリエ関数",
+                    data: [{ x: 0, y: 0 }, { x: 0, y: 1 },],
+                    showLine: true,
+                    fill: false,
+                    borderColor: 'rgb(255, 99, 132)',
+                    backgroundColor: 'rgb(255, 99, 132)'
+                }]
+        };
+        var config = {};
+        var plugins = {
+            type: 'scatter',
+            data: data,
+            options: config
+        };
+        this.chart = new Chart(ctx, plugins);
     };
-    var config = {};
-    var plugins = {
-        type: 'scatter',
-        data: data,
-        options: config
+    GraphDrawer.prototype.Plot = function (interval) {
+        var _this = this;
+        setInterval(function () {
+            _this.AppendPlot();
+            _this.chart.update();
+        }, interval);
     };
-    new Chart(ctx, plugins);
-}
+    GraphDrawer.prototype.AppendPlot = function () {
+        if (!this.chart.data.datasets) {
+            this.chart.data.datasets = [];
+        }
+        if (!this.chart.data.datasets[0].data) {
+            this.chart.data.datasets[0].data = [];
+        }
+        var plotNum = this.chart.data.datasets[0].data.length;
+        this.chart.data.datasets[0].data.push({ x: plotNum + 1, y: 1 });
+        console.log(this.chart.data.datasets[0].data);
+        console.log("Interval type1");
+    };
+    return GraphDrawer;
+}());
--- DrawGraph.ts
+++ DrawGraph.ts
@@ -1,27 +1,52 @@
-chart: Chart;
+class GraphDrawer {
+    chart: Chart;
 
-function InitializeGraph() {
-    const ctx: any = document.getElementById('ex_chart');
+    InitializeGraph(contextId: string) {
+        const ctx: any = document.getElementById(contextId);
 
-    const data = {
-        datasets: [{
-            radius: 0,
-            label: "フーリエ関数",
-            showLine: true,
-            fill: false,
-            borderColor: 'rgb(255, 99, 132)',
-            backgroundColor: 'rgb(255, 99, 132)'
-        }],
-    };
+        const data = {
+            datasets: [{
+                radius: 0,
+                label: "フーリエ関数",
+                data: [{ x: 0, y: 0 }, { x: 0, y: 1 },],
+                showLine: true,
+                fill: false,
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgb(255, 99, 132)'
+            }],
+        };
 
-    var config = {
-    };
+        var config = {
+        };
 
-    var plugins = {
-        type: 'scatter',
-        data: data,
-        options: config
-    };
+        var plugins = {
+            type: 'scatter',
+            data: data,
+            options: config
+        };
 
-    new Chart(ctx, plugins);
+        this.chart = new Chart(ctx, plugins);
+    }
+
+    public Plot(interval: number): void {
+        setInterval(() => {
+            this.AppendPlot();
+            this.chart.update();
+        }, interval);
+    }
+
+    private AppendPlot(): void {
+        if (!this.chart.data.datasets) {
+            this.chart.data.datasets = [];
+        }
+
+        if (!this.chart.data.datasets[0].data) {
+            this.chart.data.datasets[0].data = [];
+        }
+
+        var plotNum: number = this.chart.data.datasets[0].data.length;
+        (this.chart.data.datasets[0].data as {}[]).push({ x: plotNum + 1, y: 1 });
+        console.log(this.chart.data.datasets[0].data);
+        console.log("Interval type1");
+    }
 }
